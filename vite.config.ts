fcdd36d771dcbfe762ad3e7677a53e656a6d864e
Sadilson-S <sadilsonsamuel4@gmail.com>
2025-03-15 20:25:22 +0100

    Fix project configuration for Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,10 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    minify: 'terser',
+  }
 });
